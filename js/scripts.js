14f0146bc668b77e41fa52f3b9b4e1c554d7075f
Na Hyung Choi <schoinh@gmail.com>
2019-06-14 15:23:03 -0700

    handle negative number input

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,33 @@
 var input, countUpString, countDownString;
+var array = [];
 
 var beepBoop = function(name, inputNum) {
-  var array = [];
-  for (var num = 0; num <= inputNum; num++) {
-    num = num.toString();
-    if (num.includes(3)) {
-      var apology = "I'm sorry, " + name + ", I'm afraid I can't do that";
-      array.push(apology);
-    } else if (num.includes(2)) {
-      array.push("Boop!");
-    } else if (num.includes(1)) {
-      array.push("Beep!");
-    } else {
-      array.push(num);
+  array = [];
+  if (inputNum < 0) {                   // handles negative number input
+    for (var num = 0; num >= inputNum; num--) {
+      generateArray(num);
+    }
+  } else {                    // handles positive number input
+    for (var num = 0; num <= inputNum; num++) {
+      generateArray(num);
     }
   }
-  countUpString = array.join(", ");
-  countDownString = array.reverse().join(", ");
+  countUpString = array.join(", ");           // counts from zero
+  countDownString = array.reverse().join(", ");       //counts to zero
+}
+
+var generateArray = function(num) {
+  num = num.toString();
+  if (num.includes(3)) {
+    var apology = "I'm sorry, " + inputName + ", I'm afraid I can't do that";
+    array.push(apology);
+  } else if (num.includes(2)) {
+    array.push("Boop!");
+  } else if (num.includes(1)) {
+    array.push("Beep!");
+  } else {
+    array.push(num);
+  }
 }
 
 $(function() {
@@ -56,7 +67,7 @@ $(function() {
     $(".panic").show();
   })
 
-  $("#back").click(function() {
+  $("#back").click(function() {             // takes user back to main screen
     $(".panic").hide();
     $(".container").show();
   })
