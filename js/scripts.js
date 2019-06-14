2ad6d008eb8f8b18e595b4ea7097313a24c050c5
Na Hyung Choi <schoinh@gmail.com>
2019-06-14 12:05:35 -0700

    add countdown button; create apology variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,36 @@
-var input, outputArray, outputString
+var input, outputArray, outputString;
 
 var beepBoop = function(name, inputNum) {
   outputArray = [];
-  debugger
   for (var num = 0; num <= inputNum; num++) {
     num = num.toString();
     if (num.includes(3)) {
-      outputArray.push("I'm sorry")
-      outputArray.push(name)
-      outputArray.push("I'm afraid I can't do that")
+      var apology = "I'm sorry, " + name + ", I'm afraid I can't do that";
+      outputArray.push(apology);
     } else if (num.includes(2)) {
-      outputArray.push("Boop!")
+      outputArray.push("Boop!");
     } else if (num.includes(1)) {
-      outputArray.push("Beep!")
+      outputArray.push("Beep!");
     } else {
       outputArray.push(num);
     }
   }
-  outputString = outputArray.join(", ")
+  outputString = outputArray.join(", ");
 }
 
 
 $(function() {
-  $("form").submit(function(event) {
+  $("#countUp").click(function(event) {
+    event.preventDefault();
+
+    inputName = $("#name").val();
+    inputNumber = $("#input").val();
+    beepBoop(inputName, inputNumber);
+
+    $("#output").text(outputString);
+  })
+
+  $("#countDown").click(function(event) {
     event.preventDefault();
 
     inputName = $("#name").val();
