831d56258349a55e1a3add5a4396c5f11013fe80
Na Hyung Choi <schoinh@gmail.com>
2019-06-14 10:24:36 -0700

    output beep, boop, I'm sorry, for exact numbers 1, 2, and 3

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,15 @@ var input, outputArray, outputString
 var beepBoop = function(input) {
   outputArray = [];
   for (var num = 0; num <= input; num++) {
-    outputArray.push(num);
+    if (num === 3) {
+      outputArray.push("I'm sorry, Dave. I'm afraid I can't do that.")
+    } else if (num === 2) {
+      outputArray.push("Boop!")
+    } else if (num === 1) {
+      outputArray.push("Beep!")
+    } else {
+      outputArray.push(num);
+    }
   }
   outputString = outputArray.join(", ")
 }
