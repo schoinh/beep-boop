3762452f8940af1cfe73f66d092dea8919c80303
Na Hyung Choi <schoinh@gmail.com>
2019-06-14 10:38:29 -0700

    output beep, boop, I'm sorry for numbers that contain 1, 2, or 3 in any digit

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,14 @@ var input, outputArray, outputString
 
 var beepBoop = function(input) {
   outputArray = [];
+  debugger
   for (var num = 0; num <= input; num++) {
-    if (num === 3) {
+    num = num.toString();
+    if (num.includes(3)) {
       outputArray.push("I'm sorry, Dave. I'm afraid I can't do that.")
-    } else if (num === 2) {
+    } else if (num.includes(2)) {
       outputArray.push("Boop!")
-    } else if (num === 1) {
+    } else if (num.includes(1)) {
       outputArray.push("Beep!")
     } else {
       outputArray.push(num);
